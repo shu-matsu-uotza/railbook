92ef6f2b925529a0a8b4d01904300b5574106992
shu-matsu-uotza <matsuyamashuuhei@matsuyamashuuhei-no-MacBook-Air.local>
2019-03-08 01:24:17 +0900

    [add] def action (field, html5) to study for form_for

--- app/controllers/view_controller.rb
+++ app/controllers/view_controller.rb
@@ -2,4 +2,12 @@ class ViewController < ApplicationController
   def form_for
     @book = Book.new
   end
+
+  def field
+    @book = Book.new
+  end
+
+  def html5
+    @book = Book.new
+  end
 end
